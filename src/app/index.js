a077b150de9540bc1439e8b25c71eebc901abf6b
subajat1 <bayumunajat@outlook.com>
2019-05-25 14:29:57 +0800

    fix immutable state

--- src/app/index.js
+++ src/app/index.js
@@ -2,20 +2,33 @@ import {render} from "react-dom";
 import React from "react";
 import {createStore} from "redux";
 
-const reducer = (state, action) => {
+const initialState = {
+    result: 1,
+    lastValues: []
+};
+
+const reducer = (state = initialState, action) => {
     switch (action.type) {
         case "ADD":
-            state = state + action.payload;
+            state = {
+                ...state,
+                result: state.result + action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
             break;
         case "SUBTRACT":
-            state = state - action.payload;
+            state = {
+                ...state,
+                result: state.result - action.payload,
+                lastValues: [...state.lastValues, action.payload]
+            };
             break;
     }
     return state;
 };
 
 
-const store = createStore(reducer, 1);
+const store = createStore(reducer);
 
 store.subscribe(() => {
     console.log("Store updated!", store.getState());
